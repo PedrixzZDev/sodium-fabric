7100914de9942d39c69cb6d4aebfd7572aff250d
PedrixzZDev <pedrixzz6@gmail.com>
2024-10-18 09:11:42 -0300

    Update chunk_material.glsl

--- common/src/main/resources/assets/sodium/shaders/include/chunk_material.glsl
+++ common/src/main/resources/assets/sodium/shaders/include/chunk_material.glsl
@@ -1,12 +1,19 @@
 const uint MATERIAL_USE_MIP_OFFSET = 0u;
 const uint MATERIAL_ALPHA_CUTOFF_OFFSET = 1u;
 
-const float[4] ALPHA_CUTOFF = float[4](0.0, 0.1, 0.1, 1.0);
+const float ALPHA_CUTOFF_0 = 0.0;
+const float ALPHA_CUTOFF_1 = 0.1;
+const float ALPHA_CUTOFF_2 = 0.1;
+const float ALPHA_CUTOFF_3 = 1.0;
 
 float _material_mip_bias(uint material) {
-    return ((material >> MATERIAL_USE_MIP_OFFSET) & 1u) != 0u ? 0.0 : -4.0;
+    return (material & (1u << MATERIAL_USE_MIP_OFFSET)) != 0u ? 0.0 : -4.0;
 }
 
 float _material_alpha_cutoff(uint material) {
-    return ALPHA_CUTOFF[(material >> MATERIAL_ALPHA_CUTOFF_OFFSET) & 3u];
+    uint index = (material >> MATERIAL_ALPHA_CUTOFF_OFFSET) & 3u;
+    return index == 0u ? ALPHA_CUTOFF_0 :
+           index == 1u ? ALPHA_CUTOFF_1 :
+           index == 2u ? ALPHA_CUTOFF_2 :
+           ALPHA_CUTOFF_3;
 }
