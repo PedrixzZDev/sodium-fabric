81f711f3ca5ceca9ab14600865f0fa870c65cef1
PedrixzZDev <pedrixzz6@gmail.com>
2024-10-23 07:47:29 -0300

    Update ChunkTracker.java

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/map/ChunkTracker.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/map/ChunkTracker.java
@@ -4,115 +4,95 @@ import it.unimi.dsi.fastutil.longs.*;
 import net.minecraft.world.level.ChunkPos;
 
 public class ChunkTracker implements ClientChunkEventListener {
-    private final Long2IntOpenHashMap chunkStatus = new Long2IntOpenHashMap();
-    private final LongOpenHashSet chunkReady = new LongOpenHashSet();
+    private final Long2IntMap chunkStatus = new Long2IntOpenHashMap();
+    private final LongSet chunkReady = new LongOpenHashSet();
 
     private final LongSet unloadQueue = new LongOpenHashSet();
     private final LongSet loadQueue = new LongOpenHashSet();
 
-    public ChunkTracker() {
-
-    }
+    public ChunkTracker() {}
 
     @Override
-    public void updateMapCenter(int chunkX, int chunkZ) {
-
-    }
+    public void updateMapCenter(int chunkX, int chunkZ) {}
 
     @Override
-    public void updateLoadDistance(int loadDistance) {
-
-    }
+    public void updateLoadDistance(int loadDistance) {}
 
     @Override
     public void onChunkStatusAdded(int x, int z, int flags) {
-        var key = ChunkPos.asLong(x, z);
+        long key = ChunkPos.asLong(x, z);
+        int prev = chunkStatus.getOrDefault(key, 0);
+        int cur = prev | flags;
 
-        var prev = this.chunkStatus.get(key);
-        var cur = prev | flags;
+        if (prev == cur) return;
 
-        if (prev == cur) {
-            return;
-        }
-
-        this.chunkStatus.put(key, cur);
-
-        this.updateNeighbors(x, z);
+        chunkStatus.put(key, cur);
+        updateNeighbors(x, z);
     }
 
     @Override
     public void onChunkStatusRemoved(int x, int z, int flags) {
-        var key = ChunkPos.asLong(x, z);
-
-        var prev = this.chunkStatus.get(key);
+        long key = ChunkPos.asLong(x, z);
+        int prev = chunkStatus.getOrDefault(key, 0);
         int cur = prev & ~flags;
 
-        if (prev == cur) {
-            return;
-        }
+        if (prev == cur) return;
 
-        if (cur == this.chunkStatus.defaultReturnValue()) {
-            this.chunkStatus.remove(key);
+        if (cur == 0) {
+            chunkStatus.remove(key);
         } else {
-            this.chunkStatus.put(key, cur);
+            chunkStatus.put(key, cur);
         }
-
-        this.updateNeighbors(x, z);
+        updateNeighbors(x, z);
     }
 
     private void updateNeighbors(int x, int z) {
         for (int ox = -1; ox <= 1; ox++) {
             for (int oz = -1; oz <= 1; oz++) {
-                this.updateMerged(ox + x, oz + z);
+                updateMerged(ox + x, oz + z);
             }
         }
     }
 
     private void updateMerged(int x, int z) {
         long key = ChunkPos.asLong(x, z);
-
-        int flags = this.chunkStatus.get(key);
+        int flags = chunkStatus.getOrDefault(key, 0);
 
         for (int ox = -1; ox <= 1; ox++) {
             for (int oz = -1; oz <= 1; oz++) {
-                flags &= this.chunkStatus.get(ChunkPos.asLong(ox + x, oz + z));
+                flags &= chunkStatus.getOrDefault(ChunkPos.asLong(ox + x, oz + z), 0);
             }
         }
 
         if (flags == ChunkStatus.FLAG_ALL) {
-            if (this.chunkReady.add(key) && !this.unloadQueue.remove(key)) {
-                this.loadQueue.add(key);
+            if (chunkReady.add(key) && !unloadQueue.remove(key)) {
+                loadQueue.add(key);
             }
         } else {
-            if (this.chunkReady.remove(key) && !this.loadQueue.remove(key)) {
-                this.unloadQueue.add(key);
+            if (chunkReady.remove(key) && !loadQueue.remove(key)) {
+                unloadQueue.add(key);
             }
         }
     }
 
     public LongCollection getReadyChunks() {
-        return LongSets.unmodifiable(this.chunkReady);
+        return LongSets.unmodifiable(chunkReady);
     }
 
     public void forEachEvent(ChunkEventHandler loadEventHandler, ChunkEventHandler unloadEventHandler) {
-        forEachChunk(this.unloadQueue, unloadEventHandler);
-        this.unloadQueue.clear();
+        forEachChunk(unloadQueue, unloadEventHandler);
+        unloadQueue.clear();
 
-        forEachChunk(this.loadQueue, loadEventHandler);
-        this.loadQueue.clear();
+        forEachChunk(loadQueue, loadEventHandler);
+        loadQueue.clear();
     }
 
     public static void forEachChunk(LongCollection queue, ChunkEventHandler handler) {
-        var iterator = queue.iterator();
-
-        while (iterator.hasNext()) {
-            var pos = iterator.nextLong();
-
-            var x = ChunkPos.getX(pos);
-            var z = ChunkPos.getZ(pos);
-
+        queue.forEach(pos -> {
+            int x = ChunkPos.getX(pos);
+            int z = ChunkPos.getZ(pos);
             handler.apply(x, z);
-        }
+        });
     }
 
     public interface ChunkEventHandler {
